07d417244fcb680fd7d21e95af888682a456d5b0
Samer Kayali <samer.kayali@seidor.com.br>
2021-08-19 20:46:32 -0300

    Initial commit

---
+++ packages/container/src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import MarketingApp from "./components/MarketingApp";
+
+export default () => {
+  return (
+    <div>
+      <div>Hi there</div>
+      <MarketingApp />
+    </div>
+  );
+};
