d202de2a60623959985c5cebffbb839003e90f46
Samer Kayali <samerkkr@gmail.com>
2021-08-22 16:10:56 -0300

    fix: fixed css colision

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -3,13 +3,22 @@ import { BrowserRouter } from "react-router-dom";
 import MarketingApp from "./components/MarketingApp";
 import Header from "./components/Header";
 
+import {
+  StylesProvider,
+  createGenerateClassName,
+} from "@material-ui/core/styles";
+
+const generateClassName = createGenerateClassName({ productionPrefix: "co" });
+
 export default () => {
   return (
     <BrowserRouter>
-      <div>
-        <Header />
-        <MarketingApp />
-      </div>
+      <StylesProvider generateClassName={generateClassName}>
+        <div>
+          <Header />
+          <MarketingApp />
+        </div>
+      </StylesProvider>
     </BrowserRouter>
   );
 };
